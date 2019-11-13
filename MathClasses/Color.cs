d5f8f12d3da1dc41d75d36502917e3767c7cc99f
Sean Eastin <s198013@students.aie.edu>
2019-11-13 17:49:44 -0600

    Add project files.

---
+++ MathClasses/Color.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClasses
+{
+    public class Color
+    {
+        public UInt32 color;
+
+        public Color()
+        {
+            color = 0;
+        }
+
+    }
+}
